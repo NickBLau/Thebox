a2d916a507d7f9bfede336aa4e59226ac5752081
NickBLau <114568331+NickBLau@users.noreply.github.com>
2022-11-11 13:22:15 +0100

    Add files via upload

---
+++ gallery/js/slides.js
@@ -0,0 +1,49 @@
+// The slide array containing the slide elements as objects
+// Each object conteains a header, a text, a button link, a button text, a background image ande any individual styling. 
+
+const SLIDE_ARRAY = [
+   
+    // SLIDE_ARRAY[0]
+    {
+        heading: "Oregano Height",
+        text: "2464 Royal Ln. Mesa, New Jersey",
+        buttonLink: "#",
+        buttonText: " ",
+        slideImage: "gallery/images/gallery4.jpg",
+        slideStyle: ""
+
+
+    },
+    // SLIDE_ARRAY[1]
+    {
+        heading: "Wish Stone Building",
+        text: "2972 Westheimer Rd. Santa Ana, Illinois",
+        
+        buttonLink: "#",
+        buttonText: " ",
+        slideImage: "/gallery/images/gallery1.jpg",
+        slideStyle: ""
+
+    },
+    // SLIDE_ARRAY[2]
+    {
+        heading: "Mr. Parkinson's House",
+        text: "3517 W. Gray St. Uica, Pennysylvania",
+        buttonLink: "#",
+        buttonText: " ",
+        slideImage: "gallery/images/gallery3.jpg",
+        slideStyle: ""
+
+    },
+    // SLIDE_ARRAY[3]
+    {
+        heading: "Wildstone Infra Hotel",
+        text: "2715 Ash Dr. San Jose, South Dakota",
+        buttonLink: "#",
+        buttonText: " ",
+        slideImage: "gallery/images/gallery2.jpg",
+        slideStyle: ""
+
+    }
+
+];
